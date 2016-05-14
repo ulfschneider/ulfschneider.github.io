8b81ba93c1b37ea2f81d96d9a543bb2c30c20596
ulfschneider <u@ulf.codes>
2016-05-14 19:15:44 +0200

    Format css and javascript

--- r/helper.js
+++ r/helper.js
@@ -1,80 +1,80 @@
 function hasClass(selector, elem) {
-    if (selector.charAt(0) === '.') {
-        return (" ." + elem.className + " ").indexOf(" " + selector + " ") > -1;
-    } else {
-        return false;
-    }
+	if (selector.charAt(0) === '.') {
+		return (" ." + elem.className + " ").indexOf(" " + selector + " ") > -1;
+	} else {
+		return false;
+	}
 }
 
 function isTag(selector, elem) {
-    if (elem.tagName.toUpperCase() === selector.toUpperCase()) {
-        return true;
-    } else {
-        return false;
-    }
+	if (elem.tagName.toUpperCase() === selector.toUpperCase()) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function recursiveSelect(selector, collect, elem) {
-    if (elem != null) {
-        if (hasClass(selector, elem)) {
-          collect.push(elem);
-        } else if (isTag(selector, elem)) {
-          collect.push(elem);
-        }
+	if (elem != null) {
+		if (hasClass(selector, elem)) {
+			collect.push(elem);
+		} else if (isTag(selector, elem)) {
+			collect.push(elem);
+		}
 
-        var children = elem.children;
-        for (var i = 0; i < children.length; i++) {
-          recursiveSelect(selector, collect, children[i]);
-        }
-    }
-    return collect;
+		var children = elem.children;
+		for (var i = 0; i < children.length; i++) {
+			recursiveSelect(selector, collect, children[i]);
+		}
+	}
+	return collect;
 }
 
 function select(selector, elem) {
-    var objects = [];
+	var objects = [];
 
-    if (selector.charAt(0) === '#') {
-        return document.getElementById(selector.substring(1, selector.length));
-    } else {
-        return recursiveSelect(selector, objects, (elem ? elem : document.body));
-    }
+	if (selector.charAt(0) === '#') {
+		return document.getElementById(selector.substring(1, selector.length));
+	} else {
+		return recursiveSelect(selector, objects, (elem ? elem : document.body));
+	}
 }
 
 function figCaption(dom) {
-    if (dom && dom.innerText) {
+	if (dom && dom.innerText) {
 
-      var img = select("img", dom);
-      var i = 0;
-      var html = "";
-      for(i = 0; i< img.length; i++) {
-        html = html + img[i].outerHTML;
-        img[i].parentNode.removeChild(img[i]);
-      }
-      dom.innerHTML = html + "<figcaption>" + dom.innerHTML + "</figcaption>";
-    }
+		var img = select("img", dom);
+		var i = 0;
+		var html = "";
+		for (i = 0; i < img.length; i++) {
+			html = html + img[i].outerHTML;
+			img[i].parentNode.removeChild(img[i]);
+		}
+		dom.innerHTML = html + "<figcaption>" + dom.innerHTML + "</figcaption>";
+	}
 }
 
 
-(function format(){
+(function format() {
 
-	window.onscroll= function() {
+	window.onscroll = function() {
 		if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-			document.getElementById("up").style.display="inline";
+			document.getElementById("up").style.display = "inline";
 		}
 	}
-	
+
 	var i = 0;
-  	var s = select(".left");
-	for(i = 0; i < s.length; i++) {
-    figCaption(s[i]);
+	var s = select(".left");
+	for (i = 0; i < s.length; i++) {
+		figCaption(s[i]);
 	}
-  s = select(".right");
-  for(i = 0; i < s.length; i++) {
-    figCaption(s[i]);
+	s = select(".right");
+	for (i = 0; i < s.length; i++) {
+		figCaption(s[i]);
 	}
-  s = select(".double");
-  for(i = 0; i < s.length; i++) {
-    figCaption(s[i]);
-  }
+	s = select(".double");
+	for (i = 0; i < s.length; i++) {
+		figCaption(s[i]);
+	}
 
 })();
