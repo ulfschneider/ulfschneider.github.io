67d99be526910993abcc8cfa924db053548ab15b
Ulf Schneider <u@ulf.codes>
2020-04-27 21:55:20 +0200

    Precaching of site pages

--- serviceworker.js
+++ serviceworker.js
@@ -8,132 +8,39 @@ const STATIC_CACHE_MINUTES = 60 * 24; //one day
 const IMAGE_CACHE_MINUTES = 60 * 24 * 10; //cache for 10 days
 const IMAGE_CACHE_LIMIT_COUNT = 150; //cache 150 images
 
-//!!!! if you change the url, change it also in the URLS_TO_IGNORE in the offline page !!!!
-const OFFLINE_URL = '/offline/';
-
 const STATIC_CACHE_NAME = `${STATIC}-${CACHE_NAME}`;
 const IMAGE_CACHE_NAME = `${IMAGE}-${CACHE_NAME}`;
 const RUNTIME_CACHE_NAME = `${RUNTIME}-${CACHE_NAME}`;
 const CACHE_NAMES = [STATIC_CACHE_NAME, IMAGE_CACHE_NAME, RUNTIME_CACHE_NAME];
 
+//!!!! if you change the url, change it also in the URLS_TO_IGNORE in the offline page !!!!
+const OFFLINE_URL = '/offline/';
+
 const STATIC_PRECACHE_URLS = [
-    OFFLINE_URL
+    OFFLINE_URL,
+    '/',
+    '/reading/',
+    '/articles/',
+    '/tools/',
+    '/journal/',
+    '/search/',
+    '/colophon/',
+    '/ownership/',
+    'https://unpkg.com/lunr/lunr.js'
 ];
 
-
-//logging on localhost
-function devlog(message) {
-    if (location.hostname == 'localhost') {
-        console.log(message);
-    }
-}
-
-//error logging on localhost
-function deverror(message) {
-    if (location.hostname == 'localhost') {
-        console.error(message);
-
-    }
-}
-
-function setExpire(headers, minutes) {
-    minutes = minutes ? minutes : CACHE_MINUTES;
-
-    let expires = new Date();
-    expires.setMinutes(expires.getMinutes() + minutes);
-    headers.append(`${CACHE_NAME}-expires`, expires.toUTCString());
-}
-
-function getExpire(headers) {
-    const expires = headers.get(`${CACHE_NAME}-expires`);
-    return expires ? Date.parse(expires) : 0;
-}
-
-async function maintainExpiration(response, expireMinutes) {
-
-    cloneHeaders = function (response) {
-        let headers = new Headers();
-        for (var kv of response.headers.entries()) {
-            headers.append(kv[0], kv[1]);
-        }
-        return headers;
-    }
-
-    cloneResponse = async function (response, headers) {
-        try {
-            let blob = await response.blob();
-            return new Response(blob, {
-                status: response.status,
-                statusText: response.statusText,
-                headers: headers ? headers : response.headers
-            });
-        } catch (error) {
-            console.error(error);
-            return response;
-        }
-    }
-
-    if (expireMinutes > 0 && response.type != 'opaque') {
-        //unfortunately, for opaque response types 
-        //the expiration cannot be controlled here
-        let headers = cloneHeaders(response);
-        setExpire(headers, expireMinutes);
-        return cloneResponse(response, headers);
-    } else {
-        return response;
-    }
-
-}
-
-async function ensureCacheLimit(cacheName, limitCount) {
-    try {
-        if (limitCount > 0) {
-            let cache = await caches.open(cacheName);
-            let keys = await cache.keys();
-            let removeCount = keys.length - limitCount;
-            for (let i = 0; i < removeCount; i++) {
-                devlog(`Removing from ${cacheName} to ensure cache limit of ${limitCount}: ${keys[i].url}`);
-                await cache.delete(keys[i]).catch(error => console.error(error));
-            }
-        }
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function putResponseIntoCache({ request, response, cacheName, expireMinutes, limitCount }) {
-    try {
-        let metaResponse = await maintainExpiration(response, expireMinutes)
-        let cache = await caches.open(cacheName);
-        devlog(`Putting into ${cacheName}: ${request.url}`);
-        let result = cache.put(request, metaResponse);
-        await ensureCacheLimit(cacheName, limitCount);
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-function isExpired(response) {
-    const expires = getExpire(response.headers);
-
-    if (expires > 0) {
-        const now = new Date();
-        if (expires < now) {
-            return true; //response is expired
-        }
-    }
-    return false;
-}
+const NO_CACHE_URLS = [
+    '/feed.xml/'
+]
 
 
 //precache on install
 addEventListener('install', event => {
     const precache = async function () {
-        let staticCache = await caches.open(STATIC_CACHE_NAME);
+
         for (let url of STATIC_PRECACHE_URLS) {
             try {
-                await staticCache.add(url); //TODO put offline page into runtime cache
+                await fetchAndCache({ request: makeURL(url) });
             } catch (err) {
                 console.error(`Failure when adding ${url} to ${STATIC_CACHE_NAME}`, err);
             }
@@ -161,14 +68,19 @@ addEventListener('activate', event => {
 async function fetchAndCache({ request, responseFromCache }) {
 
     if (responseFromCache
-        && getExpire(responseFromCache.headers) > 0
+        && getExpire(responseFromCache) > 0
         && !isExpired(responseFromCache)) {
         //we have a cache entry that´s not expired
         //no need to bother the network
         return responseFromCache;
     }
 
-    const url = new URL(request.url);
+    if (request instanceof String || request instanceof URL) {
+        request = new Request(request);
+    }
+
+    let url = new URL(request.url);
+
     if (responseFromCache) {
         //we have a cache entry, but it´s expired 
         //or we have no expiration date for the entry
@@ -181,8 +93,14 @@ async function fetchAndCache({ request, responseFromCache }) {
     }
     return fetch(request)
         .then(async responseFromNetwork => {
-            
-            if (request.headers.get('Accept').includes('text/html')) {
+
+            if (NO_CACHE_URLS.includes(url.pathname + url.search) || NO_CACHE_URLS.includes(url)) {
+                return responseFromNetwork;
+            }
+
+            let accept = request.headers.get('Accept');
+            if (accept && accept.includes('text/html')
+                || /^\/.+\/(\?|$)/.test(url.pathname)) {
                 await putResponseIntoCache({
                     cacheName: RUNTIME_CACHE_NAME,
                     request: request,
@@ -245,10 +163,10 @@ async function cacheFirst(event) {
 
     if (responseFromCache && !isExpired(responseFromCache)) {
         devlog(`Responding from cache ${request.url}`);
-        
+
         //clone response and call without await
-        fetchAndCache({ request: request, responseFromCache: responseFromCache.clone() }) 
-            .catch(error => deverror('Failure in cache first operation ' + error)); 
+        fetchAndCache({ request: request, responseFromCache: responseFromCache.clone() })
+            .catch(error => deverror('Failure in cache first operation ' + error));
 
         return responseFromCache;
     } else {
@@ -287,3 +205,111 @@ addEventListener('fetch', event => {
 
 
 
+//// helpers 
+
+function devlog(message) {
+    if (location.hostname == 'localhost') {
+        console.log(message);
+    }
+}
+
+function deverror(message) {
+    if (location.hostname == 'localhost') {
+        console.error(message);
+
+    }
+}
+
+function makeURL(url) {
+    if (!url.startsWith('http')) {
+        return new URL(url, location.origin);
+    } else {
+        return new URL(url);
+    }
+}
+
+function getExpire(response) {
+    const expires = response.headers.get(`${CACHE_NAME}-expires`);
+    return expires ? Date.parse(expires) : 0;
+}
+
+async function maintainExpiration(response, expireMinutes) {
+
+    cloneHeaders = function (response) {
+        let headers = new Headers();
+        for (var kv of response.headers.entries()) {
+            headers.append(kv[0], kv[1]);
+        }
+        return headers;
+    }
+
+    cloneResponse = async function (response) {
+        try {
+            let headers = cloneHeaders(response);
+
+            let expires = new Date();
+            expires.setMinutes(expires.getMinutes() + expireMinutes);
+            headers.append(`${CACHE_NAME}-expires`, expires.toUTCString());
+
+            let blob = await response.blob();
+            return new Response(blob, {
+                status: response.status,
+                statusText: response.statusText,
+                headers: headers ? headers : response.headers
+            });
+        } catch (error) {
+            console.error(error);
+            return response;
+        }
+    }
+
+    if (expireMinutes > 0 && response.type != 'opaque') {
+        //unfortunately, for opaque response types 
+        //the expiration cannot be controlled here        
+        return cloneResponse(response);
+    } else {
+        return response;
+    }
+
+}
+
+async function ensureCacheLimit(cacheName, limitCount) {
+    try {
+        if (limitCount > 0) {
+            let cache = await caches.open(cacheName);
+            let keys = await cache.keys();
+            let removeCount = keys.length - limitCount;
+            for (let i = 0; i < removeCount; i++) {
+                devlog(`Removing from ${cacheName} to ensure cache limit of ${limitCount}: ${keys[i].url}`);
+                await cache.delete(keys[i]).catch(error => console.error(error));
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function putResponseIntoCache({ request, response, cacheName, expireMinutes, limitCount }) {
+    try {
+        let metaResponse = await maintainExpiration(response, expireMinutes)
+        let cache = await caches.open(cacheName);
+        devlog(`Putting into ${cacheName}: ${request.url}`);
+        let result = cache.put(request, metaResponse);
+        await ensureCacheLimit(cacheName, limitCount);
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function isExpired(response) {
+    const expires = getExpire(response);
+
+    if (expires > 0) {
+        const now = new Date();
+        if (expires < now) {
+            return true; //response is expired
+        }
+    }
+    return false;
+}
