42d036f25caba30c84c7bc216f11d1c3a24039cf
Ulf Schneider <u@ulf.codes>
2020-05-09 09:40:30 +0200

    Change FitVids

--- r/fit-vids.js
+++ r/fit-vids.js
@@ -7,50 +7,50 @@ which borrows its basic idea from Thierry Koblentz
 https://alistapart.com/article/creating-intrinsic-ratios-for-video/
 */
 
-addEventListener('load', event => {
-    // List of Video Vendors embeds you want to support
-    const players = [
-        'iframe[src*="player.vimeo.com"]',
-        'iframe[src*="youtube.com"]',
-        'iframe[src*="youtube-nocookie.com"]',
-        'iframe[src*="kickstarter.com"][src*="video.html"]',
-    ];
 
-    // Select videos
-    const fitVids = document.querySelectorAll(players.join(','));
+// List of Video Vendors embeds you want to support
+const players = [
+    'iframe[src*="player.vimeo.com"]',
+    'iframe[src*="youtube.com"]',
+    'iframe[src*="youtube-nocookie.com"]',
+    'iframe[src*="kickstarter.com"][src*="video.html"]',
+];
 
-    // If there are videos on the page...
-    if (fitVids.length) {
+// Select videos
+const fitVids = document.querySelectorAll(players.join(','));
 
-        // Loop through videos
-        for (let vid of fitVids) {
+// If there are videos on the page...
+if (fitVids.length) {
 
-            // Get Video Information            
-            var width = vid.getAttribute('width');
-            var height = vid.getAttribute('height');
-            var aspectRatio = height / width;
-            var parentDiv = vid.parentNode;
+    // Loop through videos
+    for (let vid of fitVids) {
 
-            // Wrap it in a DIV
-            var div = document.createElement('div');
-            div.style.position = 'relative';
-            div.style.paddingBottom = aspectRatio * 100 + '%';
-            div.style.width = '100%';
-            div.style.height = '0';
+        // Get Video Information            
+        var width = vid.getAttribute('width');
+        var height = vid.getAttribute('height');
+        var aspectRatio = height / width;
+        var parentDiv = vid.parentNode;
 
-            vid.style.position = 'absolute';
-            vid.style.top = '0';
-            vid.style.left = '0';
-            vid.style.width = '100%';
-            vid.style.height = '100%';
+        // Wrap it in a DIV
+        var div = document.createElement('div');
+        div.style.position = 'relative';
+        div.style.paddingBottom = aspectRatio * 100 + '%';
+        div.style.width = '100%';
+        div.style.height = '0';
 
-            parentDiv.insertBefore(div, vid);
-            vid.remove();
-            div.appendChild(vid);
+        vid.style.position = 'absolute';
+        vid.style.top = '0';
+        vid.style.left = '0';
+        vid.style.width = '100%';
+        vid.style.height = '100%';
 
-            // Clear height/width from fitVid
-            vid.removeAttribute('height')
-            vid.removeAttribute('width');
-        }
+        parentDiv.insertBefore(div, vid);
+        vid.remove();
+        div.appendChild(vid);
+
+        // Clear height/width from fitVid
+        vid.removeAttribute('height')
+        vid.removeAttribute('width');
     }
-});
+}
+
